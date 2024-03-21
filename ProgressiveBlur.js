2b302cb38660d533ee96498e87754376690fccb9
RiyuDio <132917131+RiyuDio@users.noreply.github.com>
2024-03-21 03:51:11 -0500

    Update ProgressiveBlur.js

--- ProgressiveBlur.js
+++ ProgressiveBlur.js
@@ -1,78 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var parentElements = document.querySelectorAll('[ProgressiveBlur="parent"]');
-    parentElements.forEach(function(parentElement) {
-        var insetValue = '0';
-        var progressiveColor = parentElement.getAttribute('ProgressiveColor') || '#FFFFFF';
-
-        if (progressiveColor.toLowerCase() === 'invisible') {
-            // If ProgressiveColor is set to "invisible", do not add the additionalDiv
-            var addAdditionalDiv = false;
-        } else {
-            var addAdditionalDiv = true;
-            // Convert hex color to RGB
-            var red = parseInt(progressiveColor.substring(1, 3), 16);
-            var green = parseInt(progressiveColor.substring(3, 5), 16);
-            var blue = parseInt(progressiveColor.substring(5, 7), 16);
-        }
-
-        Object.assign(parentElement.style, {
-            zIndex: 5,
-            inset: insetValue, 
-            position: 'relative',
-            pointerEvents: 'none'
-        });
-
-        var direction = parentElement.getAttribute('ProgressiveDirection') || 'down';
-
-        var blurValues = [0.5, 1, 2, 4, 8, 16, 32];
-        var maskPercentages = [
-            [0, 12.5, 25, 37.5],
-            [12.5, 25, 37.5, 50],
-            [25, 37.5, 50, 62.5],
-            [37.5, 50, 62.5, 75],
-            [50, 62.5, 75, 87.5],
-            [62.5, 75, 87.5, 100]
-        ];
-
-        // Adjust mask percentages based on direction
-        if (direction === 'up') {
-            maskPercentages.reverse();
-            insetValue = '-10% 0 0 0'; // Adjust inset for up direction
-        } else if (direction === 'down') {
-            insetValue = '0 0 -10% 0'; // Adjust inset for down direction
-        }
-
-        // Create and append the required number of divs
-        for (var i = 0; i < 6; i++) {
-            var div = document.createElement('div');
-            Object.assign(div.style, {
-                position: 'absolute',
-                inset: insetValue, // Apply the inset value based on direction
-                zIndex: i + 2,
-                backdropFilter: 'blur(' + blurValues[i] + 'px)',
-            });
-
-            // Add mask only if not using Safari
-            if (!isSafari()) {
-                div.style.maskImage = `linear-gradient(to bottom, rgba(0, 0, 0, 0) ${maskPercentages[i][0]}%, rgba(0, 0, 0, 1) ${maskPercentages[i][1]}%, rgba(0, 0, 0, 1) ${maskPercentages[i][2]}%, rgba(0, 0, 0, 0) ${maskPercentages[i][3]}%)`;
-            } else {
-                div.style.webkitMaskImage = `linear-gradient(to bottom, rgba(0, 0, 0, 0) ${maskPercentages[i][0]}%, rgba(0, 0, 0, 1) ${maskPercentages[i][1]}%, rgba(0, 0, 0, 1) ${maskPercentages[i][2]}%, rgba(0, 0, 0, 0) ${maskPercentages[i][3]}%)`;
-                div.style.webkitBackdropFilter = 'blur(' + blurValues[i] + 'px)';
-            }
-            parentElement.appendChild(div);
-        }
-
-        // Trigger repaint after a delay
-        setTimeout(function() {
-            parentElement.style.display = "table";
-            parentElement.offsetHeight;
-            parentElement.style.display = "block";
-        }, 100); // Adjust the delay as needed
-
-        // Add ::before and ::after pseudo-elements via CSS
-        var styleSheet = document.createElement('style');
-        styleSheet.type = 'text/css';
-        styleSheet.innerText = `
+document.addEventListener("DOMContentLoaded",function(){document.querySelectorAll('[ProgressiveBlur="parent"]').forEach(function(e){var r="0",t=e.getAttribute("ProgressiveColor")||"#FFFFFF";if("invisible"===t.toLowerCase())var a=!1;else var a=!0,i=parseInt(t.substring(1,3),16),$=parseInt(t.substring(3,5),16),n=parseInt(t.substring(5,7),16);Object.assign(e.style,{zIndex:5,inset:r,position:"relative",pointerEvents:"none"});var o=e.getAttribute("ProgressiveDirection")||"down",s=[.5,1,2,4,8,16,32],b=[[0,12.5,25,37.5],[12.5,25,37.5,50],[25,37.5,50,62.5],[37.5,50,62.5,75],[50,62.5,75,87.5],[62.5,75,87.5,100]];"up"===o?(b.reverse(),r="-10% 0 0 0"):"down"===o&&(r="0 0 -10% 0");for(var l=0;l<6;l++){var g=document.createElement("div");Object.assign(g.style,{position:"absolute",inset:r,zIndex:l+2,backdropFilter:"blur("+s[l]+"px)"}),isSafari()?(g.style.webkitMaskImage=`linear-gradient(to bottom, rgba(0, 0, 0, 0) ${b[l][0]}%, rgba(0, 0, 0, 1) ${b[l][1]}%, rgba(0, 0, 0, 1) ${b[l][2]}%, rgba(0, 0, 0, 0) ${b[l][3]}%)`,g.style.webkitBackdropFilter="blur("+s[l]+"px)"):g.style.maskImage=`linear-gradient(to bottom, rgba(0, 0, 0, 0) ${b[l][0]}%, rgba(0, 0, 0, 1) ${b[l][1]}%, rgba(0, 0, 0, 1) ${b[l][2]}%, rgba(0, 0, 0, 0) ${b[l][3]}%)`,e.appendChild(g)}setTimeout(function(){e.style.display="table",e.offsetHeight,e.style.display="block"},100);var d=document.createElement("style");if(d.type="text/css",d.innerText=`
             .gradient-blur::before, .gradient-blur::after {
                 position: absolute;
                 inset: 0;
@@ -86,28 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 z-index: 8;
                 backdrop-filter: blur(64px);
                 -webkit-backdrop-filter: blur(64px);
-                mask-image: linear-gradient(to bottom, rgba(0, 0, 0, 0) 87.5%, rgba(${red}, ${green}, ${blue}, 1) 100%);
-                -webkit-mask-image: linear-gradient(to bottom, rgba(0, 0, 0, 0) 87.5%, rgba(${red}, ${green}, ${blue}, 1) 100%);
+                mask-image: linear-gradient(to bottom, rgba(0, 0, 0, 0) 87.5%, rgba(${i}, ${$}, ${n}, 1) 100%);
+                -webkit-mask-image: linear-gradient(to bottom, rgba(0, 0, 0, 0) 87.5%, rgba(${i}, ${$}, ${n}, 1) 100%);
             }
-        `;
-        document.head.appendChild(styleSheet);
-
-        // Add an additional div with linear gradient only if not invisible
-        if (addAdditionalDiv) {
-            var additionalDiv = document.createElement('div');
-            var gradientColors = direction === 'up' ? `rgb(${red}, ${green}, ${blue}, 100) 20%, rgba(${red}, ${green}, ${blue}, 0) 30%, rgba(${red}, ${green}, ${blue}, 0)` : `rgba(${red}, ${green}, ${blue}, 0) 10%, rgba(${red}, ${green}, ${blue}, 0) 90%, rgba(${red}, ${green}, ${blue}, 100) 80%`;
-            Object.assign(additionalDiv.style, {
-                position: 'absolute',
-                inset: '0', 
-                zIndex: 1, 
-                backgroundImage: `linear-gradient(${gradientColors})`
-            });
-            parentElement.appendChild(additionalDiv);
-        }
-    });
-});
-
-// Function to check if the browser is Safari
-function isSafari() {
-    return /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-}
+        `,document.head.appendChild(d),a){var u=document.createElement("div"),_="up"===o?`rgb(${i}, ${$}, ${n}, 100) 20%, rgba(${i}, ${$}, ${n}, 0) 30%, rgba(${i}, ${$}, ${n}, 0)`:`rgba(${i}, ${$}, ${n}, 0) 10%, rgba(${i}, ${$}, ${n}, 0) 90%, rgba(${i}, ${$}, ${n}, 100) 80%`;Object.assign(u.style,{position:"absolute",inset:"0",zIndex:1,backgroundImage:`linear-gradient(${_})`}),e.appendChild(u)}})});function isSafari(){return/^((?!chrome|android).)*safari/i.test(navigator.userAgent)}
